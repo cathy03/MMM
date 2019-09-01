9c506173896ebd9a4a38d21f0896d51142044051
KAKAO <kakao@aidens-MacBook-Pro.local>
2019-09-01 19:20:41 +0900

    x

--- js/film_info.js
+++ js/film_info.js
@@ -17,14 +17,14 @@ var film_info = [
     date: '2018',
     title: 'WHIPLASH',
     person: 'Damien Chazelle<br>Miles Teller, J.K. Simmons',
-    poster: 'poster/poster_01.html'
+    poster: 'poster/poster_03.html'
   },
-  
+
   { genre: 'ACTION<br>SCI-FI',
     date: '2011',
     title: 'RISE OF THE PLANET OF THE APES',
     person: 'Rupert Wyatt<br>Andy Serkis',
-    poster: 'poster/poster_01.html'
+    poster: 'poster/poster_04.html'
   }
 ]
 
